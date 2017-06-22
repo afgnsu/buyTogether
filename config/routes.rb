166e0d0067acfbca688ac3e406eebf3ca4e06b58
arfullight <tmimvhuang@gmail.com>
2017-06-22 14:55:56 +0800

    course 23

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   root 'pages#homepage'
 
   get '/stores' => 'stores#index'
+
+  # create new store
+  get  '/stores/new' => 'stores#new'
+  post '/stores'     => 'stores#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
