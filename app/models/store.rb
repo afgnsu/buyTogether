6dea8d878271dfeb922a610db6cd23510a4a9c72
arfullight <tmimvhuang@gmail.com>
2017-06-27 15:28:51 +0800

    course 31

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,7 @@
 class Store < ApplicationRecord
+  # carreirwave settings
+  mount_uploader :logo, LogoUploader
+  mount_uploader :image, StoreImageUploader
+
   validates_presence_of :name, :phone, :address, :description
 end
