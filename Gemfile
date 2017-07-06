257a13306acd20d5aac8badd8a351d8e171efb25
arfullight <tmimvhuang@gmail.com>
2017-07-06 12:53:59 +0800

    course 47

--- Gemfile
+++ Gemfile
@@ -42,6 +42,9 @@ gem 'carrierwave', '~> 1.0'
 # authentication
 gem 'devise'
 
+# form
+gem 'cocoon'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -7,7 +7,7 @@ class PurchasesController < ApplicationController
 
   def show
     @purchase = Purchase.find params[:id]
-    @order    = Order.find_by(user: current_user, purchase: @purchase)
+    @order    = Order.find_by(user: current_user, purchase: @purchase) || Order.new
   end
 
   def new
